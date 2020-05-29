8d47e997584e43bf0073fc7194f8af8a88628d4b
abacaxiguy <jlucasoc@hotmail.com>
2020-05-29 20:43:48 -0300

    ✔ Add toggle navbar animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,7 @@
-let controller,
-    slideScene,
-    pageScene,
-    mouse = document.querySelector(".cursor"),
-    mouseTxt = mouse.querySelector("span");
+let controller, slideScene, pageScene;
+const mouse = document.querySelector(".cursor"),
+    mouseTxt = mouse.querySelector("span"),
+    burger = document.querySelector(".burger");
 
 function animateSlides() {
     // Init controller
@@ -98,6 +97,41 @@ function activeCursor(e) {
     }
 }
 
+function navToggle(e) {
+    if (!e.target.classList.contains("active")) {
+        e.target.classList.add("active");
+        gsap.to(".line1", 0.5, {
+            rotate: "45",
+            y: 5,
+            backgroundColor: "black",
+        });
+        gsap.to(".line2", 0.5, {
+            rotate: "-45",
+            y: -5,
+            backgroundColor: "black",
+        });
+        gsap.to("#logo", 0.5, { color: "black" });
+        gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
+        document.body.classList.add("hide");
+    } else {
+        e.target.classList.remove("active");
+        gsap.to(".line1", 0.5, {
+            rotate: "0",
+            y: 0,
+            backgroundColor: "white",
+        });
+        gsap.to(".line2", 0.5, {
+            rotate: "0",
+            y: 0,
+            backgroundColor: "white",
+        });
+        gsap.to("#logo", 0.5, { color: "white" });
+        gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%)" });
+        document.body.classList.remove("hide");
+    }
+}
+
+burger.addEventListener("click", navToggle);
 window.addEventListener("mousemove", cursor);
 window.addEventListener("mouseover", activeCursor);
 
